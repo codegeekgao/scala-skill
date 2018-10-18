066ee10b7ad1e343bcad3fb5036a678eb747bfed
codegeekgao <codegeek_mrgao@163.com>
2018-10-18 15:11:25 +0800

    :sparkles: scala的option学习

---
+++ src/main/scala/collection/optional/OptionalDemo.scala
@@ -0,0 +1,15 @@
+package collection.optional
+
+/**
+  * OptionalDemo 学习
+  *
+  * @author codegeekgao
+  * @version Id: OptionalDemo.scala, v 0.1 2018/10/18 15:09 codegeekgao Exp $$
+  */
+object OptionalDemo {
+
+  def main(args: Array[String]): Unit = {
+    val str = null
+    print(Option(str).isEmpty)
+  }
+}
